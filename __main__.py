754438ce141047eec7adc048ccb2b328754d79e9
drwuro <email@drwuro.com>
2025-01-23 21:13:17 +0100

    added cool effect

--- __main__.py
+++ __main__.py
@@ -37,9 +37,14 @@ GRID_WIDTH = 11
 HEX_WIDTH = 24
 HEX_HEIGHT = 8
 
+clock = pygame.time.Clock()
+tick = 0
+
 
 while running:
     output.fill((0, 0, 0))
+
+    gridcolor = ledwall.brightness((0, 128 + ((tick * 2) % 128) if (tick * 2) % 256 < 128 else (255 - ((tick * 2) % 128)), 0))
 
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
@@ -56,17 +61,17 @@ while running:
 
             px1, py1 = getCoords(x, y)
             px2, py2 = getCoords(x, y+1)
-            pygame.draw.line(output, ledwall.brightness((0,128,0)), (px1, py1), (px2, py2))
+            pygame.draw.line(output, gridcolor, (px1, py1), (px2, py2))
 
             if y % 4 == 0:
                 px1, py1 = getCoords(x, y)
                 px2, py2 = getCoords(x-1, y+1)
-                pygame.draw.line(output, ledwall.brightness((0,128,0)), (px1, py1), (px2, py2))
+                pygame.draw.line(output, gridcolor, (px1, py1), (px2, py2))
 
             if y % 4 == 2:
                 px1, py1 = getCoords(x, y)
                 px2, py2 = getCoords(x+1, y+1)
-                pygame.draw.line(output, ledwall.brightness((0,128,0)), (px1, py1), (px2, py2))
+                pygame.draw.line(output, gridcolor, (px1, py1), (px2, py2))
 
     ledwall.font_huge.centerText(output, 'HEXGRID', y=2, fgcolor=ledwall.brightness((0, 255, 0)))
     ledwall.compose()
@@ -92,3 +97,6 @@ while running:
                 ledwall.enableOverlay(not ledwall.showOverlay)
             elif e.key == pygame.K_F11:
                 pygame.display.toggle_fullscreen()
+
+    clock.tick(60)
+    tick += 1
